9bdd85d465ab13a674d197c080ca8b00ae5236be
Adewale Lawal <87212727+wazatarbass@users.noreply.github.com>
2022-06-22 23:07:42 +0100

    another commit

--- I4G014330MJX/blog/models.py
+++ I4G014330MJX/blog/models.py
@@ -1,3 +1,14 @@
+from time import timezone
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text =  models.TextField(max_length=300)
+    Author = models.ForeignKey('auth.User', on_delete=models.CASCADE,)
+    Created_date = models.DateTimeField(auto_now_add=True)
+    Published_date =models.DateTimeField()
+    
+    def __str__(self):
+     return self.Title
+    
